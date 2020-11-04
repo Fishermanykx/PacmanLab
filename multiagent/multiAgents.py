5daf140ddf784516e9133ee4084b2a84b9e055ba
Fisher <fishermanykx.mail@gmail.com>
2020-11-04 17:07:50 +0800

    ReflexAgent的简单实现，仅考虑与食物和ghost，未考虑pacman吃药的情况

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -1,16 +1,16 @@
+# -*- coding: utf-8 -*-
 # multiAgents.py
 # --------------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
 
 from util import manhattanDistance
 from game import Directions
@@ -18,8 +18,9 @@ import random, util
 
 from game import Agent
 
+
 class ReflexAgent(Agent):
-    """
+  """
       A reflex agent chooses an action at each choice point by examining
       its alternatives via a state evaluation function.
 
@@ -28,9 +29,8 @@ class ReflexAgent(Agent):
       headers.
     """
 
-
-    def getAction(self, gameState):
-        """
+  def getAction(self, gameState):
+    """
         You do not need to change this method, but you're welcome to.
 
         getAction chooses among the best options according to the evaluation function.
@@ -38,21 +38,25 @@ class ReflexAgent(Agent):
         Just like in the previous project, getAction takes a GameState and returns
         some Directions.X for some X in the set {North, South, West, East, Stop}
         """
-        # Collect legal moves and successor states
-        legalMoves = gameState.getLegalActions()
+    # Collect legal moves and successor states
+    legalMoves = gameState.getLegalActions()
 
-        # Choose one of the best actions
-        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
-        bestScore = max(scores)
-        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+    # Choose one of the best actions
+    scores = [
+        self.evaluationFunction(gameState, action) for action in legalMoves
+    ]
+    bestScore = max(scores)
+    bestIndices = [
+        index for index in range(len(scores)) if scores[index] == bestScore
+    ]
+    chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
 
-        "Add more of your code here if you want to"
+    "Add more of your code here if you want to"
 
-        return legalMoves[chosenIndex]
+    return legalMoves[chosenIndex]
 
-    def evaluationFunction(self, currentGameState, action):
-        """
+  def evaluationFunction(self, currentGameState, action):
+    """
         Design a better evaluation function here.
 
         The evaluation function takes in the current and proposed successor
@@ -66,28 +70,58 @@ class ReflexAgent(Agent):
         Print out these variables to see what you're getting, then combine them
         to create a masterful evaluation function.
         """
-        # Useful information you can extract from a GameState (pacman.py)
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
-        newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
-        newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+    # Useful information you can extract from a GameState (pacman.py)
+    successorGameState = currentGameState.generatePacmanSuccessor(action)
+    # print("Successor game score:", successorGameState.data.score)
+    newPos = successorGameState.getPacmanPosition()
+    newFood = successorGameState.getFood()
+    newFoodList = newFood.asList()
+    # print("newFood", newFood.data)
+    newGhostStates = successorGameState.getGhostStates()
+    # a = newGhostStates[0]
+    # print a
+    # exit(0)
+    newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
-        "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+    "*** YOUR CODE HERE ***"
+    # 简单实现，仅考虑与食物和ghost的距离，未考虑吃药的情况
+    evaluated_score = 0
+
+    # 计算 pacman 与食物的最近距离
+    min_dist = -1
+    for food in newFoodList:
+      dist = util.manhattanDistance(newPos, food)
+      if min_dist == -1 or min_dist > dist:
+        min_dist = dist
+
+    # 计算 pacman 与 ghost 的总距离
+    ghost_dists = 1
+    adj_ghost_num = 0
+    for ghost in newGhostStates:
+      dist = util.manhattanDistance(newPos, ghost.configuration.pos)
+      ghost_dists += dist
+      if dist <= 1:
+        adj_ghost_num += 1
+
+    evaluated_score = successorGameState.getScore(
+    ) + 1 / float(min_dist) - 1 / float(ghost_dists) - adj_ghost_num
+
+    return evaluated_score
+
 
 def scoreEvaluationFunction(currentGameState):
-    """
+  """
       This default evaluation function just returns the score of the state.
       The score is the same one displayed in the Pacman GUI.
 
       This evaluation function is meant for use with adversarial search agents
       (not reflex agents).
     """
-    return currentGameState.getScore()
+  return currentGameState.getScore()
+
 
 class MultiAgentSearchAgent(Agent):
-    """
+  """
       This class provides some common elements to all of your
       multi-agent searchers.  Any methods defined here will be available
       to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
@@ -101,18 +135,19 @@ class MultiAgentSearchAgent(Agent):
       is another abstract class.
     """
 
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
-        self.index = 0 # Pacman is always agent index 0
-        self.evaluationFunction = util.lookup(evalFn, globals())
-        self.depth = int(depth)
+  def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+    self.index = 0  # Pacman is always agent index 0
+    self.evaluationFunction = util.lookup(evalFn, globals())
+    self.depth = int(depth)
+
 
 class MinimaxAgent(MultiAgentSearchAgent):
-    """
+  """
       Your minimax agent (question 2)
     """
 
-    def getAction(self, gameState):
-        """
+  def getAction(self, gameState):
+    """
           Returns the minimax action from the current gameState using self.depth
           and self.evaluationFunction.
 
@@ -128,46 +163,49 @@ class MinimaxAgent(MultiAgentSearchAgent):
           gameState.getNumAgents():
             Returns the total number of agents in the game
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
-    """
+  """
       Your minimax agent with alpha-beta pruning (question 3)
     """
 
-    def getAction(self, gameState):
-        """
+  def getAction(self, gameState):
+    """
           Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
-    """
+  """
       Your expectimax agent (question 4)
     """
 
-    def getAction(self, gameState):
-        """
+  def getAction(self, gameState):
+    """
           Returns the expectimax action using self.depth and self.evaluationFunction
 
           All ghosts should be modeled as choosing uniformly at random from their
           legal moves.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
 
 def betterEvaluationFunction(currentGameState):
-    """
+  """
       Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
       evaluation function (question 5).
 
       DESCRIPTION: <write something here so we know what you did>
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+  "*** YOUR CODE HERE ***"
+  util.raiseNotDefined()
+
 
 # Abbreviation
 better = betterEvaluationFunction
-
